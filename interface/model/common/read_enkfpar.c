300acfc4b669d6f6e11eb374c88a7bae08f96c89
sunnydays99 <150025643+sunnydays99@users.noreply.github.com>
2026-05-13 14:24:45 +0200

    Update read_enkfpar.c

--- interface/model/common/read_enkfpar.c
+++ interface/model/common/read_enkfpar.c
@@ -90,6 +90,7 @@ void read_enkfpar(char *parname)
   clmwatmin_switch      = iniparser_getint(pardict,"CLM:watmin_switch",0);
   clmswc_mask_snow      = iniparser_getint(pardict,"CLM:swc_mask_snow",0);
   clmupdate_tws         = iniparser_getint(pardict,"CLM:update_tws",0);
+  clmupdate_sif         = iniparser_getint(pardict,"CLM:update_sif",0);
 
   /* get settings for COSMO */
   nproccosmo      = iniparser_getint(pardict,"COSMO:nprocs",0);
